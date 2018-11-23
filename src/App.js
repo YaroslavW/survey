ec4b2ce2128aec2332e6c85a9c8627c755f6bdd0
YaroslavW <doctor355@yahoo.com>
2018-11-23 11:01:02 +0300

    Input and welcome fields completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      id:'',
+      name:'',
+      answers:{
+        q1:'',
+        q2:'',
+        q3:'',
+        q4:''
+      },
+      submitted: false
+    }
+  }
+  handleNameSubmit(event){
+    var name =this.refs.name.value; 
+    this.setState({ name: name},
+      ()=>console.log(this.state))
+    // console.log(this.state)
+    event.preventDefault()
+  }
   render() {
+    var user;
+    var questions;
+    if(this.state.name && this.state.submitted === false){
+      user=<h2>Welcome {this.state.name}</h2>
+    } else if(!this.state.name && this.state.submitted === false){
+      user= <span>
+        <h2>Please enter your name to begin the survey.</h2>
+        <form onSubmit={this.handleNameSubmit.bind(this)}>
+          <input type="text" placeholder="Enter name..." ref="name"/>
+        </form>
+      </span>
+      questions = ''
+    } else if(this.state.submitted === true){
+
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Simple Survey</h1>
+
         </header>
+        <div className="text-center">
+          {user}
+        </div>
       </div>
     );
   }
